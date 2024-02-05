41da655431c4dd48b422fc67f5a79384950407e3
Yoonge <7884008@qq.com>
2024-02-05 23:43:43 +0800

    Update fn search.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
         process::exit(1);
     }
     println!("Search {} in file {}", args[1], args[2]);
+    search(&args[1], &args[2]);
 }
 
 
@@ -15,3 +16,15 @@ fn main() {
 //     filename: String,
 //     case_insensitive: bool,
 // }
+
+fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let lines: Vec<&str> = contents.lines().collect();
+    for line in lines {
+        if line.contains(query) {
+            println!("{}", line);
+            results.push(line);
+        }
+    }
+    results
+}
