eccfeadb835f3def00fd8398f2d499a145235cce
Yoonge <7884008@qq.com>
2024-02-24 16:57:21 +0800

    Optimizing the search fn.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # ⌨️  Minigrep
 
-[![Minigrep Rust](https://img.shields.io/badge/Minigrep-Rust-orange)](https://github.com/yoonge/minigrep) ![version](https://img.shields.io/badge/version-0.2.0-yellow) [![license](https://img.shields.io/badge/license-MIT-blue)](./LICENSE) ![rust](https://img.shields.io/badge/rust-1.76.0-green.svg) ![cargo](https://img.shields.io/badge/cargo-1.76.0-orange.svg)
+[![Minigrep Rust](https://img.shields.io/badge/Minigrep-Rust-orange)](https://github.com/yoonge/minigrep) ![version](https://img.shields.io/badge/version-0.2.0-yellow) [![license](https://img.shields.io/badge/license-MIT-blue)](./LICENSE) ![rust](https://img.shields.io/badge/rust-1.76.0-green.svg) ![cargo](https://img.shields.io/badge/cargo-1.76.0-purple.svg)
 
 
 ## 💡 Introduction
--- src/lib.rs
+++ src/lib.rs
@@ -21,34 +21,19 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
-    let results = if config.case_sensitive {
-        search(&config.query, &contents)
-    } else {
-        search_case_insensitive(&config.query, &contents)
-    };
+    let results = search(&config.query, &contents, config.case_sensitive);
     for line in results {
         println!("{}", line);
     }
     Ok(())
 }
 
-pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
-}
-
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    let query = query.to_lowercase();
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+fn search<'a>(query: &str, contents: &'a str, case_sensitive: bool) -> Vec<&'a str> {
+    contents.lines()
+        .filter(|line| if case_sensitive {
+                line.contains(query)
+            } else {
+                line.to_lowercase().contains(&query.to_lowercase())
+            })
+        .collect()
 }
